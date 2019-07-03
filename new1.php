10b25c58cfda5a295bb182e0bc49e876b3c600bc
kavita <kavita.linuxbean@gmail.com>
2019-07-03 22:49:31 +0530

    adding interview questions and answers file

---
+++ new1.php
@@ -0,0 +1,10 @@
+1 cookies, session diff.
+2 get & post method in php
+3 how to open system file in php code
+4 $arr = ("a" ,"b", "c", "d");
+how to find b exist in array
+5 aggregate function sum, min, max, avg,
+6. join left join, right join
+7 array push and array pop
+8 how to find grouped adding in left join
+
